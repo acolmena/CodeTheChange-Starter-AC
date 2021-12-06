74a58152f52ef987a49e09d7909edd747a3f4c14
acolmena <95594394+acolmena@users.noreply.github.com>
2021-12-05 16:19:54 -0800

    Add files via upload

---
+++ precache-manifest.152570a0fea3b9c0c20baf798b8fc249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c095c4381a9e0b4f3514279fbc87269d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0644d13f6220b364c2a6",
+    "url": "/static/css/main.63f40a23.chunk.css"
+  },
+  {
+    "revision": "b7ca4fca6849f1aecdb7",
+    "url": "/static/js/2.e8de3a73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8de3a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0644d13f6220b364c2a6",
+    "url": "/static/js/main.d7cb98ba.chunk.js"
+  },
+  {
+    "revision": "0c30d17efcb1aed1916c",
+    "url": "/static/js/runtime-main.ad8ac308.js"
+  }
+]);
